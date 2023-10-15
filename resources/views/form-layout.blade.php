fe04def27c842d4f6923029078273feaf2153e89
Juanito-boop <acevedojuanesteban.colombia@gmail.com>
2023-10-15 01:23:30 -0500

    agregando, los estilos a los archivos que faltaron (1)

--- resources/views/form-layout.blade.php
+++ resources/views/form-layout.blade.php
@@ -8,7 +8,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}"> <!-- Esto es para que funcione el formulario -->
   <link rel="icon" type="image/png" href="{{ asset('img/image-removebg-preview.svg') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  {{-- <link rel="stylesheet" href="{{ asset('css/login_y_registro.css') }}"> --}}
+  <link rel="stylesheet" href="https://npuxpuelimayqrsmzqur.supabase.co/storage/v1/object/public/images/css/output.css">
   @vite('resources/css/app.css')
   <title>@yield('title')</title>
 </head>
