8aff47006fc52d3daa06e3a08082aa575924c58d
Juanito-boop <acevedojuanesteban.colombia@gmail.com>
2023-10-17 05:11:31 -0500

    ok

--- resources/views/components/testimonials.blade.php
+++ resources/views/components/testimonials.blade.php
@@ -1,4 +1,4 @@
-<section class="testimonials text-color3">
+<section class="">
 	<div class="m-5 rounded-[10px] bg-colorPrincipal_1 p-5 text-start">
 		<h2 class="font-poppins font-bold">
 			DESCUBRE UN POCO DE NOSOTROS
@@ -6,30 +6,30 @@
 		<h3 class="font-poppins font-semibold">
 			SOMOS VINOS DE LA VILLA
 		</h3>
-		<p class="font-poppins text-color3">
+		<p class="font-poppins">
 			Estamos ubicados en Villa De Leyva boyaca Carrera 9 #11-47 Segundo piso de La Galleta, Villa de Leyva,
 			Boyaca, Colombia
 		</p>
-		<p class="font-poppins text-color3">
+		<p class="font-poppins">
 			Un lugar para los amantes del vino. Vino por copa desde COP$8000, Botellas
 			desde COP$27.900, Amplio surtido en vinos de: Colombia, España, Italia, Francia, Chile, Argentina y
 			California para acompañar: Tapas, Paninis, tablas de quesos y jamones. Buena música. Ambiente agradable.
 		</p>
-		<p class="font-poppins text-color3">
-			Poseemos diferentes servicios como: Bar de vinos. También podemos ofrecer servicio de restaurante como:
-			Cenas, Abierto hasta tarde (revisar horario según corresponda),
-			Bebidas
-		</p>
-		<p class="font-poppins text-color3">
-			OTRAS CARACTERÍSTICAS: Comodos Asientos, Brindamos el fino alcohol, wifi gratis, Aceptamos tarjetas de
-			crédito, Servicio de mesa, Reservas, Acceso para silla de ruedas, Vino y cerveza
-		</p>
-		<p class="font-poppins text-color3">HORARIOS DE ATENCION :</p>
-		<ul>
-			<li class="font-poppins text-color3">dom 12:00 p.m. - 10:00 p.m.</li>
-			<li class="font-poppins text-color3">lun-mar-mié-jue 3:00 p.m. - 10:00 p.m.</li>
-			<li class="font-poppins text-color3">vie-sáb 12:00 p.m. - 12:00 a.m.</li>
-		</ul>
+		<p class="font-poppins">Poseemos diferentes servicios como:</p>
+		<ol>
+			<li>
+				* Bar de vinos
+			</li>
+			<li>
+				* Cenas
+			</li>
+			<li>
+				* Abierto hasta tarde (revisar horario según corresponda)
+			</li>
+			<li>
+				* Bebidas
+			</li>
+		</ol>
 	</div>
 	<div class="m-5 flex flex-row justify-center gap-20 rounded-[10px] bg-color2 p-5 text-center">
 		<div class="rounded-[10px] p-4 hover:bg-[rgb(122,122,122)]">
